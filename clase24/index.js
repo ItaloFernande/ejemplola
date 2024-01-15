469800568d3c7b49f0e0e7da59ba79ff9e1e1f59
ItaloFernande <italofernandez777@gmail.com>
2024-01-15 12:50:24 -0500

    clase del lunes semana 2

---
+++ clase24/index.js
@@ -0,0 +1,28 @@
+function main()
+{
+    let num = 10
+    let nombre = "Edgaricius"
+    let SerHuman = true
+    let altura = 1.80
+    let listado = [1,2,3,4,6]
+    let alumno = 
+    {
+        nombre : "Gorilicius",
+        codigo : "20210983",
+        alumnaSistemas : false,
+        hobbies : ["leer mangas","programar bananas","hacer tops de mejores intros"],
+        sexo : "Si",
+        universidad : {
+            nombre : "Universidad de Lima",
+            direccion : "La casa de Daniel taype"
+        }
+    } 
+    num = 777
+    
+    let datoNulo = null
+    let variableIndefinida = undefined
+    console.log(alumno)
+    console.log(num)
+}
+
+main()
---
+++ clase24/memoria.js
@@ -0,0 +1,46 @@
+let simbolosDispo =["&#127814;","&#128507;","&#128511;"]
+
+let listaCasillas = []
+
+function crearCasillas()
+{
+    for(let i =0;i < simbolosDispo.length;i++)
+    {
+        let casilla1 = {
+            simbolo : simbolosDispo[i],
+            monstrandoSimbolo : false
+        }
+        let casilla2 = {
+            simbolo : simbolosDispo[i],
+            monstrandoSimbolo : false
+        }
+        listaCasillas.push(casilla1)
+        listaCasillas.push(casilla2)
+    }
+}
+
+function devolverCasilla(row, col)
+{
+    const pos = (row * 3) + col
+    return listaCasillas[pos]
+}
+
+function ponerSimbolosCasillas(){
+    for( let i = 0;i<2;i++)
+    {
+        for(let j = 0;j<3;j++)
+        {
+            const doometio = document.getElementById(i+"-"+j)
+            const casilla = devolverCasilla(i,j)
+            doometio.innerHTML = casilla.simbolo
+        }
+    }
+}
+
+function main()
+{
+    crearCasillas()
+    ponerSimbolosCasillas()
+}
+
+main()
